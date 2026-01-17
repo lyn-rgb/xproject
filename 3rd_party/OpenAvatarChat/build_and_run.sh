8a6709cc5f9c097e6bed42ee01d0d9039e6d15fd
lyn-rgb <2633634964@qq.com>
2026-01-18 02:09:33 +0800

    Vendor OpenAvatarChat source

---
+++ 3rd_party/OpenAvatarChat/build_and_run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+CONFIG_PATH=""
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -config | --config )
+            CONFIG_PATH="$2"
+            shift 2
+            ;;
+    esac
+done
+
+echo "${CONFIG_PATH}"
+
+docker build \
+    --build-arg CONFIG_FILE=${CONFIG_PATH}  \
+    -t open-avatar-chat:0.0.1 . 
+docker run --rm --gpus all -it --name open-avatar-chat \
+    --network=host \
+    -v `pwd`/build:/root/open-avatar-chat/build \
+    -v `pwd`/models:/root/open-avatar-chat/models \
+    -v `pwd`/ssl_certs:/root/open-avatar-chat/ssl_certs \
+    -v `pwd`/config:/root/open-avatar-chat/config \
+    -v `pwd`/models/musetalk/s3fd-619a316812/:/root/.cache/torch/hub/checkpoints/ \
+    -e NVIDIA_VISIBLE_DEVICES=all \
+    -e NVIDIA_DRIVER_CAPABILITIES=compute,video,utility \
+    -p 8282:8282 \
+    open-avatar-chat:0.0.1 \
+    --config ${CONFIG_PATH}
